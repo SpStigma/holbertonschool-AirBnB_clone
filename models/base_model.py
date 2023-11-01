d3bcf94b6d11568a01b0482baa30d2bc2e056c51
SpStigma <yoann.rivet0@gmail.com>
2023-11-01 10:56:01 +0100

    Fix: indentation of setattr.

--- models/base_model.py
+++ models/base_model.py
@@ -48,12 +48,12 @@ class BaseModel:
             updated_at (datetime): The date and time when the instance was
             last updated (initialized to the creation time).
         """
-        if kwargs is not None:
+        if kwargs:
             for key, val in kwargs.items():
                 if key != '__class__':
                     if key in ['created_at', 'updated_at']:
                         val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, val)
+                    setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
