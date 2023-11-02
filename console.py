5f6faf8d1d5b0fa1baf9807987f34be619682722
Erkenoss <xavier.bertin6@gmail.com>
2023-11-02 14:18:45 +0100

    change doc string

--- console.py
+++ console.py
@@ -35,7 +35,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_quit(self, arg):
-        """quit command to exit the program"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
