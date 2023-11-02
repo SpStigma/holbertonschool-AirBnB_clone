843dac20ab1b7d55d7580356375f8436e3079ab2
Erkenoss <xavier.bertin6@gmail.com>
2023-11-02 15:19:50 +0100

    add import in console

--- console.py
+++ console.py
@@ -17,6 +17,11 @@ import cmd
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.review import Review
+from models.place import Place
 
 
 storage = FileStorage()
